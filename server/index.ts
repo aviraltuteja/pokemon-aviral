51fddfee78bcaa3535f6dd07b1ef3e3fc0b0c455
aviralkaksha <aviraltuteja@gmail.com>
2024-05-10 06:46:28 +0530

    tRPC basic setup

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,7 @@
 import { procedure, router } from "./trpc";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export const appRouter = router({
   getPokemon: procedure.query(() => {
@@ -9,4 +12,16 @@ export const appRouter = router({
       sprite: "https://pokemon.com/pictures/bulbasaur.png",
     };
   }),
+  fetchPokemonByName: procedure
+    .input(z.object({ name: z.string() }))
+    .query(async ({ input }) => {
+      const pokemonData = await prisma.pokemon.findMany({
+        where: {
+          name: input.name,
+        },
+      });
+      return pokemonData;
+    }),
 });
+
+export type appRouter = typeof appRouter;
